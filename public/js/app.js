8c138ee094bc10504f351113263da2bb7501be09
David <dieavid01@gmail.com>
2020-05-17 03:41:11 +0100

    optimized for dark mode

--- public/js/app.js
+++ public/js/app.js
@@ -1,103 +1,39 @@
 const app = () => {
-    const toggle = document.getElementById("toggle");
+    // get dark-mode value on page reload from local storage
+    let darkMode = localStorage.getItem('dark-mode');
 
-    const darkMode = () => {
-        const container = document.querySelector('.container');
-        const dashboard = document.querySelector('.dashboard');
-        const dashboardHeader = document.querySelector('header h3');
-        const scMedia = document.querySelectorAll('.sc-media');
-        const username = document.querySelectorAll('.username');
-        const MediaText = document.querySelectorAll('.sc-media p:nth-child(3)');
-        const overviewHeader = document.querySelector('.overview h3');
-        const card = document.querySelectorAll('.card');
-        const activity = document.querySelectorAll('.activity');
-        const activityNo = document.querySelectorAll('.activity-no');
-        const attribution = document.querySelector('.attribution');
-        const attributionLink = document.querySelectorAll('.attribution a');
+    const darkModeToggle = document.querySelector('#toggle');
 
-        // convert the nodelists to arrays
-        const scMediaArray = [...scMedia];
-        const cardArray = [...card];
-        const activityArray = [...activity];
-        const activityNoArray = [...activityNo];
-        const usernameArray = [...username];
-        const MediaTextArray = [...MediaText];
-        const attributionLinkArray = [...attributionLink];
-
-        // add dark mode class to classes with single element
-        if (toggle.checked) {
-            container.classList.add('drkmd');
-            dashboard.classList.add('drkmd');
-            dashboardHeader.classList.add('drkmd');
-            overviewHeader.classList.add('drkmd');
-            attribution.classList.add('drkmd');
-        } else {
-            container.classList.remove('drkmd');
-            dashboard.classList.remove('drkmd');
-            dashboardHeader.classList.remove('drkmd');
-            overviewHeader.classList.remove('drkmd');
-            attribution.classList.remove('drkmd');
-        }
-
-        // add dark mode to classes with multiple classes
-        scMediaArray.forEach(element => {
-            if (toggle.checked) {
-                element.classList.add('drkmd');
-            } else {
-                element.classList.remove('drkmd');
-            }
-        });
-
-        cardArray.forEach(element => {
-            if (toggle.checked) {
-                element.classList.add('drkmd');
-            } else {
-                element.classList.remove('drkmd');
-            }
-        });
-
-        activityArray.forEach(element => {
-            if (toggle.checked) {
-                element.classList.add('drkmd');
-            } else {
-                element.classList.remove('drkmd');
-            }
-        });
-
-        activityNoArray.forEach(element => {
-            if (toggle.checked) {
-                element.classList.add('drkmd');
-            } else {
-                element.classList.remove('drkmd');
-            }
-        });
-
-        usernameArray.forEach(element => {
-            if (toggle.checked) {
-                element.classList.add('drkmd');
-            } else {
-                element.classList.remove('drkmd');
-            }
-        });
-
-        MediaTextArray.forEach(element => {
-            if (toggle.checked) {
-                element.classList.add('drkmd');
-            } else {
-                element.classList.remove('drkmd');
-            }
-        });
-
-        attributionLinkArray.forEach(element => {
-            if (toggle.checked) {
-                element.classList.add('drkmd');
-            } else {
-                element.classList.remove('drkmd');
-            }
-        });
+    const enableDarkMode = () => {
+        // add darkmode class to body
+        document.body.classList.add('darkmode');
+        localStorage.setItem('dark-mode', 'enabled');
+        console.log(darkMode);
     }
 
-    toggle.addEventListener('click', darkMode);
+    const disableDarkMode = () => {
+        // remove darkmode class from body
+        document.body.classList.remove('darkmode');
+        localStorage.setItem('dark-mode', 'disabled');
+        console.log(darkMode);
+    }
+
+    // The webpage loads in light mode by default. 
+    // check if darkmode is enabled on page reload in local storage (so as display the webpage as darkmode as selected by the user on the last visit to the webpage).
+    if (darkMode === 'enabled') {
+        enableDarkMode();
+    }
+
+    darkModeToggle.addEventListener('click', () => {
+        // update variable for local storage
+        darkMode = localStorage.getItem('dark-mode');
+
+        if (darkMode !== 'enabled') {
+            enableDarkMode();
+        } else {
+            disableDarkMode();
+        }
+    })
 }
 
 app();
